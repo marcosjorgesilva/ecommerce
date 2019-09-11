a120e2c4853cb4f380500e308edc527d39e64922
Marcos Jorge <marcos.jorge1996@gmail.com>
2019-09-11 12:56:14 -0300

    Criacao do projeto

---
+++ src/main/java/com/marcosjorge/projetojweb/ProjetojwebApplication.java
@@ -0,0 +1,13 @@
+package com.marcosjorge.projetojweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojwebApplication.class, args);
+	}
+
+}
