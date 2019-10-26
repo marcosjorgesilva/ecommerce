a43ee1e0640bd138601d54cb8acbfe00df5f033a
Marcos Jorge <marcos.jorge1996@gmail.com>
2019-10-26 04:10:17 -0300

    Implementando estrutura de testes para a camada repositorio.

---
+++ src/test/java/com/marcosjorge/projetojweb/repositories/RepoClienteTest.java
@@ -0,0 +1,14 @@
+package com.marcosjorge.projetojweb.repositories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RepoClienteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/marcosjorge/projetojweb/repositories/RepoPedidoTest.java
@@ -0,0 +1,14 @@
+package com.marcosjorge.projetojweb.repositories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RepoPedidoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
